849138a3fd69f0e31fd5279bc7fa24553d2f1ac0
Steven Carvalho Jr <stevencarvalho@ORRHS.ORG>
2020-02-12 10:57:01 -0500

    added codeto go to main page

--- Clicker Game/app.js
+++ Clicker Game/app.js
@@ -20,3 +20,17 @@ function youWon(){
     heading.html('You Won!')
     bananaImg.remove();
 }
+
+// let button;
+// function setup() {
+//   createCanvas(400, 400);
+//   button = createButton('Go To Main Page');
+//   button.position(width / 2, height / 2)
+//   button.mousePressed(goToLink)
+// }
+// function draw() {
+//   background(220);
+// }
+// function goToLink() {
+//   window.location.href = 'main.html'; // Same tab
+// }
