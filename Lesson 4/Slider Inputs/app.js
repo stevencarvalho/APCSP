73ef7d3e38624199bb79b8e99fb959002c2f0ac4
Steven Carvalho Jr <stevencarvalho@ORRHS.ORG>
2020-02-14 09:39:12 -0500

    made slider

---
+++ Lesson 4/Slider Inputs/app.js
@@ -0,0 +1,28 @@
+// let slider;
+// function setup() {
+//     slider = createSlider(0, 360, 100);
+//     createCanvas(600, 120); 
+//     colorMode(HSB);
+// }
+  
+// function draw(){
+//     background(slider.value(), slider.value(), slider.value());
+//     textSize(80);
+//       text("hello", 210, 100);
+//   }
+
+let slider;
+let textInput
+
+function setup() {
+    slider = createSlider(0, 360, 100);
+    textInput = createInput("Write Something Here");
+    createCanvas(600, 120); 
+    colorMode(HSB);
+}
+function draw(){
+  background(slider.value(), 100, 100);
+  textSize(80);
+    text(textInput.value(), 210, 100);
+}
+
---
+++ Lesson 4/app.js
@@ -0,0 +1,50 @@
+// let a = 3
+// let b = 7
+// a = b
+// b = a
+// console.log(a)
+// console.log(b)
+// num1 = b
+
+// num1 = num1 + 10;
+// console.log(num1);
+
+// let  someNum = 7;
+// someNum = someNum + 1;
+// console.log("The value of someNum is now: " + someNum);
+// someNum = someNum + 1;
+// console.log("The value of someNum is now: " + someNum);
+// someNum = someNum + 1;
+// console.log("The value of someNum is now: " + someNum);
+// someNum = someNum * someNum;
+// console.log("The value of someNum is now: " + someNum);
+
+// let x = 2
+// let y = 3
+
+// // your code here
+
+// y = y + x
+// console.log("The value of y is: " + y + " (y should be 5)");
+
+// let x = 7;
+// let y = 3;
+// y = x - y
+
+// // your code here
+// console.log("The value of y is: " + y + " (y should be 4)");
+
+// let x = 2;
+// let y = 3;
+// x = x + y + x
+// // your code here
+// console.log("The value of x is: " + x + " (x should be 7)");
+
+// let x = 2;
+// x = x * x * x
+// // your code here
+// console.log("The value of x is: " + x + " (x should be 8)");
+
+
+
+
